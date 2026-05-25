e8b28d162af82a124b48158183f05779895a8060
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-05-25 20:05:46 +0530

    feat: add Navbar component for improved navigation and search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { Toaster } from "react-hot-toast";
 import ProtectedRoute from "./components/protectedRoute";
 import PublicRoute from "./components/publicRoute";
 import SelectRole from "./pages/SelectRole";
+import Navbar from "./components/navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route element={<PublicRoute />}>
           <Route path="/login" element={<Login />} />
